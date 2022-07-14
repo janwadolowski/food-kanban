681f30bffa1b1497ebcdce7e9a69a992c960c6d0
Jan Wadolowski <janwadolowski@gmail.com>
2022-07-15 00:43:21 +0200

    feat: initial commit with meals app added

---
+++ meals_app/meal_enums.py
@@ -0,0 +1,6 @@
+ class YearInSchool(models.TextChoices):
+        FRESHMAN = 'FR', _('Freshman')
+        SOPHOMORE = 'SO', _('Sophomore')
+        JUNIOR = 'JR', _('Junior')
+        SENIOR = 'SR', _('Senior')
+        GRADUATE = 'GR', _('Graduate')
